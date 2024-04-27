b847622671bf5427701b3d44ef3dd5ce391132b8
NSANZIMURA <marcnsanzimfura20@gmail.com>
2024-04-27 02:00:41 +0000

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-"""Script that fetches https://alx-intranet.hbtn.io/status."""
-from urllib.request import Request, urlopen
+"""Fetch
+https://intranet.hbtn.io/status
+using urlib package
+"""
 
+import urllib.request
 
-if _name_ == "_main_":
-    req = Request("https://alx-intranet.hbtn.io/status")
-    with urlopen(req) as response:
-        body = response.read()
+if __name__ == '__main__':
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as request:
+        response = request.read()
         print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode("utf-8")))
+        print("\t- type: {}".format(type(response)))
+        print("\t- content: {}".format(response))
+        print("\t- utf8 content: {}".format(response.decode('UTF-8')))
