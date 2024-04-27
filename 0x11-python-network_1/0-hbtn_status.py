384f269cee1251d6ea63b84cd2ae24ba3e3ad7f8
NSANZIMURA <marcnsanzimfura20@gmail.com>
2024-04-27 01:46:04 +0000

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,7 +3,7 @@
 from urllib.request import Request, urlopen
 
 
-if __name__ == "__main__":
+if _name_ == "_main_":
     req = Request("https://alx-intranet.hbtn.io/status")
     with urlopen(req) as response:
         body = response.read()
@@ -11,4 +11,3 @@ if __name__ == "__main__":
         print("\t- type: {}".format(type(body)))
         print("\t- content: {}".format(body))
         print("\t- utf8 content: {}".format(body.decode("utf-8")))
-
